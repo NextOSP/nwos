c087925f5bfd57bff0a4b5959586b412a3603b01
Binh Ong <binh.ong@nextwaves.com>
2026-07-09 14:11:11 +0700

    Add addon batch 6

---
+++ addons/l10n_es_edi_sii/models/certificate.py
@@ -0,0 +1,11 @@
+from nwos import fields, models
+
+
+class CertificateCertificate(models.Model):
+    _inherit = 'certificate.certificate'
+
+    scope = fields.Selection(
+        selection_add=[
+            ('sii', 'SII')
+        ],
+    )
---
+++ addons/l10n_fi/models/account_journal.py
@@ -0,0 +1,12 @@
+# Part of NextOSP. See LICENSE file for full copyright and licensing details.
+from nwos import models, fields
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    invoice_reference_model = fields.Selection(selection_add=[
+        ('fi', 'Finnish Standard Reference (2024000068)'),
+        ('fi_rf', 'Finnish Creditor Reference (RF) (RF952024000071)'),
+    ], ondelete={'fi': lambda recs: recs.write({'invoice_reference_model': 'nwos'}),
+                 'fi_rf': lambda recs: recs.write({'invoice_reference_model': 'nwos'})})
